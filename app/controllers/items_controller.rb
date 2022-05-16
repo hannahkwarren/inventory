0c5cb583ee2f8fa50522da817bf514da167596cf
Hannah Warren <hannahkwarren@gmail.com>
2022-05-15 22:51:38 -0400

    Item create tests, controller, and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,26 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+
+    if @item.save
+      flash[:success] = "#{@item.name} created!"
+      redirect_to item_path(@item)
+    else
+      flash[:alert] = "Error: #{error_message(@item.errors)}"
+      redirect_to new_item_path
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :weight_lbs)
+  end
+
 end
---
+++ spec/items/new_spec.rb
@@ -0,0 +1,20 @@
+# spec/items/new_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'New Item form', type: :feature do
+  it 'can create a new item' do
+    visit new_item_path
+
+    fill_in :name, with: 'LOreal Hair Dye'
+    fill_in :description, with: 'Neon Pink'
+    fill_in 'Weight (In Pounds):', with: 0.5
+
+    click_button 'Create Item'
+
+    expect(page).to have_content('Details:')
+    expect(page).to have_content('LOreal Hair Dye')
+    expect(page).to have_content('Description: Neon Pink')
+    expect(page).to have_content('Weight: 0.5 lbs')
+  end
+
+end
